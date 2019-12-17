2dd75e288d7fc27bed307ffe3bfcaac88edd106b
megafabr <megafabr@yandex.ru>
2019-12-17 21:49:45 +0300

    Comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :events
+  has_many :comments, dependent: :destroy
 
   before_validation :set_name, on: :create
 
