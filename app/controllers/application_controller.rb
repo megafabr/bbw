6125971825439c75cdf4f63ac1de6d5c07defe20
megafabr <megafabr@yandex.ru>
2019-12-20 11:41:46 +0300

    name as strong parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,13 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(
       :account_update,
-      keys: [:password, :password_confirmation, :current_password, :name]
+      keys: [:password, :password_confirmation, :current_password]
     )
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
